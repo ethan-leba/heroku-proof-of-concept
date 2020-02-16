1d1a1a7393ed343bf5cdff330656ffc14d01784d
ethan-leba <ethanleba5@gmail.com>
2020-02-16 09:50:25 -0500

    new path

--- src/backend_api/handler.clj
+++ src/backend_api/handler.clj
@@ -3,10 +3,15 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def notfound "Not Found")
+
+(defn handle-submit [request] (str request))
+
 (defroutes app-routes
   (GET "/api/" [] "DATA")
+  (GET "/api/submit" request (handle-submit request))
   (route/resources "/public/")
-  (route/not-found "Not Found"))
+  (route/not-found notfound))
 
 (def app
   (wrap-defaults app-routes site-defaults))
